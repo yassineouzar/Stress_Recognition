433ef9f1581563748c1630baa91a5993215d8a62
yassineouzar <35693810+yassineouzar@users.noreply.github.com>
2022-04-07 13:29:01 +0200

    Add files via upload

---
+++ inference_preprocess.py
@@ -0,0 +1,194 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+import datetime
+
+import numpy as np
+import cv2
+from skimage.util import img_as_float
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import time
+import scipy.io
+from scipy.sparse import spdiags
+from sklearn import linear_model
+try:
+    from PIL import Image as pil_image
+except ImportError:
+    pil_image = None
+
+try:
+    from keras import backend as K
+except:
+    pass
+def array_to_img(x, data_format=None, scale=True):
+    """Converts a 3D Numpy array to a PIL Image instance.
+    # Arguments
+        x: Input Numpy array.
+        data_format: Image data format.
+        scale: Whether to rescale image values
+            to be within [0, 255].
+    # Returns
+        A PIL Image instance.
+    # Raises
+        ImportError: if PIL is not available.
+        ValueError: if invalid `x` or `data_format` is passed.
+    """
+    if pil_image is None:
+        raise ImportError('Could not import PIL.Image. '
+                          'The use of `array_to_img` requires PIL.')
+    x = np.asarray(x, dtype=K.floatx())
+    if x.ndim != 3:
+        raise ValueError('Expected image array to have rank 3 (single image). '
+                         'Got array with shape:', x.shape)
+
+    if data_format is None:
+        data_format = K.image_data_format()
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('Invalid data_format:', data_format)
+
+    # Original Numpy array x has format (height, width, channel)
+    # or (channel, height, width)
+    # but target PIL image has format (width, height, channel)
+    if data_format == 'channels_first':
+        x = x.transpose(1, 2, 0)
+    if scale:
+        x = x + max(-np.min(x), 0)
+        x_max = np.max(x)
+        if x_max != 0:
+            x /= x_max
+        x *= 255
+    if x.shape[2] == 3:
+        # RGB
+        return pil_image.fromarray(x.astype('uint8'), 'RGB')
+    elif x.shape[2] == 1:
+        # grayscale
+        return pil_image.fromarray(x[:, :, 0].astype('uint8'), 'L')
+    else:
+        raise ValueError('Unsupported channel number: ', x.shape[2])
+
+
+def img_to_array(img, data_format=None):
+    """Converts a PIL Image instance to a Numpy array.
+    # Arguments
+        img: PIL Image instance.
+        data_format: Image data format.
+    # Returns
+        A 3D Numpy array.
+    # Raises
+        ValueError: if invalid `img` or `data_format` is passed.
+    """
+    if data_format is None:
+        data_format = K.image_data_format()
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('Unknown data_format: ', data_format)
+    # Numpy array x has format (height, width, channel)
+    # or (channel, height, width)
+    # but original PIL image has format (width, height, channel)
+    x = np.asarray(img, dtype=K.floatx())
+    if len(x.shape) == 3:
+        if data_format == 'channels_first':
+            x = x.transpose(2, 0, 1)
+    elif len(x.shape) == 2:
+        if data_format == 'channels_first':
+            x = x.reshape((1, x.shape[0], x.shape[1]))
+        else:
+            x = x.reshape((x.shape[0], x.shape[1], 1))
+    else:
+        raise ValueError('Unsupported image shape: ', x.shape)
+    return x
+
+def preprocess_raw_video(videoFilePath, dim=160):
+
+    #########################################################################
+    # set up
+    t = []
+    i = 0
+    print(videoFilePath)
+    vidObj = cv2.VideoCapture(videoFilePath)
+    totalFrames = int(vidObj.get(cv2.CAP_PROP_FRAME_COUNT)) # get total frame size
+    fps = vidObj.get(cv2.CAP_PROP_FPS)
+    # calculate dusration of the video
+    print(totalFrames, round(fps))
+    # duration = totalFrames / fps
+    # print("Duration: ", duration)
+    #video_time = str(datetime.timedelta(seconds=duration))
+    #print("Video time: ", video_time)
+    Xsub = np.zeros((totalFrames, dim, dim, 3), dtype = np.float32)
+    height = vidObj.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = vidObj.get(cv2.CAP_PROP_FRAME_WIDTH)
+    success, img = vidObj.read()
+    dims = img.shape
+    #print("Orignal Height", height)
+    #print("Original width", width)
+    #########################################################################
+    # Crop each frame size into dim x dim
+    while success:
+        t.append(vidObj.get(cv2.CAP_PROP_POS_MSEC))# current timestamp in milisecond
+        vidLxL = cv2.resize(img_as_float(img[:, int(width/2)-int(height/2 + 1):int(height/2)+int(width/2), :]), (dim, dim), interpolation = cv2.INTER_AREA)
+        vidLxL = cv2.rotate(vidLxL, cv2.ROTATE_90_CLOCKWISE) # rotate 90 degree
+        vidLxL = cv2.cvtColor(vidLxL.astype('float32'), cv2.COLOR_BGR2RGB)
+        vidLxL[vidLxL > 1] = 1
+        vidLxL[vidLxL < (1/255)] = 1/255
+        Xsub[i, :, :, :] = vidLxL
+        success, img = vidObj.read() # read the next one
+        i = i + 1
+    #plt.imshow(Xsub[0])
+    #plt.title('Sample Preprocessed Frame')
+    #plt.show()
+    #print(len(Xsub))
+    #########################################################################
+    # Normalized Frames in the motion branch
+    normalized_len = len(t)
+    dXsub = np.zeros((normalized_len, dim, dim, 3), dtype = np.float32)
+    for j in range(normalized_len - 1):
+        dXsub[j, :, :, :] = (Xsub[j+1, :, :, :] - Xsub[j, :, :, :]) / (Xsub[j+1, :, :, :] + Xsub[j, :, :, :])
+    dXsub = dXsub / np.std(dXsub)
+    #########################################################################
+    # Normalize raw frames in the apperance branch
+    Xsub = Xsub - np.mean(Xsub)
+    Xsub = Xsub  / np.std(Xsub)
+    Xsub = Xsub[:totalFrames-1, :, :, :]
+    #########################################################################
+    # Plot an example of data after preprocess
+    dXsub = np.concatenate((dXsub, Xsub), axis = 3);
+
+    # img = dXsub.reshape(-1, dim, dim, 3)
+    #
+    # for i in range(len(dXsub)):
+    #     cv2.imshow('img', img[i])
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+
+    return dXsub
+# from matplotlib import pyplot as plt
+# plt.imshow(data, interpolation='nearest')
+# plt.show()
+def detrend(signal, Lambda):
+    """detrend(signal, Lambda) -> filtered_signal
+    This function applies a detrending filter.
+    This code is based on the following article "An advanced detrending method with application
+    to HRV analysis". Tarvainen et al., IEEE Trans on Biomedical Engineering, 2002.
+    *Parameters*
+      ``signal`` (1d numpy array):
+        The signal where you want to remove the trend.
+      ``Lambda`` (int):
+        The smoothing parameter.
+    *Returns*
+      ``filtered_signal`` (1d numpy array):
+        The detrended signal.
+    """
+    signal_length = signal.shape[0]
+
+    # observation matrix
+    H = np.identity(signal_length)
+
+    # second-order difference matrix
+
+    ones = np.ones(signal_length)
+    minus_twos = -2 * np.ones(signal_length)
+    diags_data = np.array([ones, minus_twos, ones])
+    diags_index = np.array([0, 1, 2])
+    D = spdiags(diags_data, diags_index, (signal_length - 2), signal_length).toarray()
+    filtered_signal = np.dot((H - np.linalg.inv(H + (Lambda ** 2) * np.dot(D.T, D))), signal)
+    return filtered_signal
