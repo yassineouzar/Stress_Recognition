90c927fc9a82fc536f29f4dbc2a4116ddb3ac877
yassineouzar <35693810+yassineouzar@users.noreply.github.com>
2022-04-14 15:53:10 +0200

    Update iPPG_prediction.py

--- iPPG_prediction.py
+++ iPPG_prediction.py
@@ -1,10 +1,11 @@
-#python code/predict_vitals.py --video_path "test-video/esra.mp4"
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
 import matplotlib
 import sklearn
 import tensorflow as tf
 import numpy as np
 import scipy.io
-import os
 import sys
 import argparse
 import csv
@@ -20,40 +21,28 @@ from scipy.signal import find_peaks, stft, lfilter, butter, welch, resample
 import matplotlib.pyplot as plt
 import pandas as pd
 
-size=18
-params = {'legend.fontsize': 'large',
-          'figure.figsize': (20,8),
-          'axes.labelsize': size,
-          'axes.titlesize': size,
-          'xtick.labelsize': size*0.75,
-          'ytick.labelsize': size*0.75,
-          'axes.titlepad': 25}
-plt.rcParams.update(params)
-
-def NormalizeData(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
-
-
-#plt.tick_params(labelsize=16)
-def predict_vitals(args):
+def predict_vitals(vid_dir, sampling_rate, batch_size, path_to_save_ippg, path_GT_BVP):
     img_rows = 36
     img_cols = 36
     frame_depth = 10
     model_checkpoint = 'mtts_can.hdf5'
-    batch_size = args.batch_size
-    fs = args.sampling_rate
-    sample_data_path = "/media/bousefsa1/My Passport/BD PPG/2 bases publiques/ubfc_phys/s1/vid_s1_T1.avi"
+    #batch_size = args.batch_size
+    fs = sampling_rate
+    #sample_data_path = "G:/F001_T1.mkv"
+    sample_data_path = vid_dir
 
     dXsub = preprocess_raw_video(sample_data_path, dim=36)
+    #print('dXsub shape', dXsub.shape)
 
     dXsub_len = (dXsub.shape[0] // frame_depth)  * frame_depth
     dXsub = dXsub[:dXsub_len, :, :, :]
+    #print("video", dXsub)
     print("dxsublen", dXsub_len)
 
     model = MTTS_CAN(frame_depth, 32, 64, (img_rows, img_cols, 3))
     model.load_weights(model_checkpoint)
 
-    f = pd.read_csv("/media/bousefsa1/My Passport/BD PPG/2 bases publiques/ubfc_phys/s1/bvp_s1_T1.csv")
+    f = pd.read_csv(path_GT_BVP)
 
     secs = len(f) / 64  # Number of seconds in signal X
     samps = secs * 35  # Number of samples to downsampqle
@@ -63,19 +52,17 @@ def predict_vitals(args):
     print("GT", len(gtData2))
     gtFloat = gtData2.astype(np.float)
 
-    gtFloat_norm= NormalizeData(gtFloat[0:6000])
-
     gtPRvalues = []
     estPRvalues = []
 
     # Calculating HR rate without SNR formulas
     # we used the estimated values and ground truth values obtained on thirty-second video segments with the starting
     # points at one-second intervals for each video
-    for i in range(0, dXsub_len - 6000, 15):
+    for i in range(0, dXsub_len - 6300, 15):
         print("finished")
 
-        yptest = model.predict((dXsub[i:i+6000, :, :, :3], dXsub[i:i+6000, :, :, -3:]), batch_size=batch_size, verbose=0)
-        gtPR = gtFloat[i:i+6000]
+        yptest = model.predict((dXsub[i:i+6300, :, :, :3], dXsub[i:i+6300, :, :, -3:]), batch_size=batch_size, verbose=0)
+        gtPR = gtFloat[i:i+6300]
         gtPRmean = np.mean(gtPR)
         gtPRvalues = np.append(gtPRvalues, gtPRmean)
         # print(gtPRvalues)
@@ -83,48 +70,35 @@ def predict_vitals(args):
 
         pulse_pred = yptest[0]
 
+
         pulse_pred = detrend(np.cumsum(pulse_pred), 100)
         [b_pulse, a_pulse] = butter(1, [0.75 / fs * 2, 2.5 / fs * 2], btype='bandpass')
         pulse_pred = scipy.signal.filtfilt(b_pulse, a_pulse, np.double(pulse_pred))
+        #print(path_to_save_ippg)
+        #print(pulse_pred)
+        pulse_pred.tofile(path_to_save_ippg, sep='\n')
 
-        pulse_pred_norm = NormalizeData(pulse_pred)
+def vid2ippg(dataset_dir, sampling_rate, batch_size):
 
-        # Peaks detection
-        indexes_peaks, _ = scipy.signal.find_peaks(pulse_pred_norm, distance=10, height=0.05)
-        indexes_peaks1, _ = scipy.signal.find_peaks(gtFloat_norm.ravel(), distance=10, height=0.05)
+    list_dir = sorted(os.listdir(dataset_dir))
+    extension = ".zip"
+    #for i in list_dir:
+    for i in range(34, len(list_dir)):
+        if not list_dir[i].endswith(extension):  # check for ".zip" extension
+            path_to_vid = os.path.join(dataset_dir, list_dir[i])
+            #print(path_to_vid)
+            vid_files = [filename for filename in sorted(os.listdir(path_to_vid))if filename.endswith(".avi")]
 
-        plt.plot(indexes_peaks, pulse_pred_norm[indexes_peaks], 'r.', linewidth=5, markersize=10, label="Peaks")
-        plt.plot(indexes_peaks1, gtFloat_norm[indexes_peaks1], 'r.', linewidth=5, markersize=10)
-
-        plt.plot(gtFloat_norm, 'g', label="Ground truth PPG")
-        plt.plot(pulse_pred_norm, 'b', label="Recovered signal by MTTS-CAN",linewidth=2)
-        plt.xlabel('Frames', fontsize = 18)
-        plt.ylabel('Recovered iPPG signal', fontsize = 18)
-        plt.legend()
-        plt.show()
-
-        pxx, frequency = matplotlib.pyplot.psd(pulse_pred_norm, NFFT=len(pulse_pred_norm), Fs=35, window=np.hamming(len(pulse_pred)))
-
-        LL_PR = 40  #lower limit pulse rate
-        UL_PR = 240
-
-
-        FMask = (frequency >= (LL_PR / 60)) & (frequency <= (UL_PR / 60))
-
-        FRange = frequency[FMask]
-        PRange = pxx[FMask]
-        MaxInd = np.argmax(PRange)
-        PR_F = FRange[MaxInd]
-        PR = PR_F * 60
-        estPRvalues = np.append(estPRvalues, PR)
+            for vid in vid_files:
+                vid_dir = os.path.join(path_to_vid, vid)
+                path_to_save_ippg = (os.path.splitext(vid_dir)[0] + ".csv").replace("vid", "ippg")
+                path_GT_BVP = (os.path.splitext(vid_dir)[0] + ".csv").replace("vid", "bvp")
+                #print(path_GT_BVP, path_to_save_ippg)
+                predict_vitals(vid_dir, sampling_rate, batch_size, path_to_save_ippg, path_GT_BVP)
 
 
-if __name__ == "__main__":
+dataset_dir = '/media/bousefsa1/My Passport/BD PPG/2 bases publiques/ubfc_phys'
+batch_size = 10
+sampling_rate = 35
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--video_path', type=str, help='processed video path')
-    parser.add_argument('--sampling_rate', type=int, default = 35, help='sampling rate of your video')
-    parser.add_argument('--batch_size', type=int, default = 100, help='batch size (multiplier of 10)')
-    args = parser.parse_args()
-
-    predict_vitals(args)
+vid2ippg(dataset_dir, sampling_rate, batch_size)
