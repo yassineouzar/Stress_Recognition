6a2188051426fc64f74bf67ea44bf36bb04d245b
yassineouzar <35693810+yassineouzar@users.noreply.github.com>
2022-04-14 10:56:50 +0200

    Update features_extraction_TL.py

--- features_extraction_TL.py
+++ features_extraction_TL.py
@@ -10,36 +10,42 @@ from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.models import Model
 from pickle import dump
 
+
 # load model
 model = VGG16()
 # remove the output layer
 model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
-
 def Features_extraction(dataset_dir, path_pkl):
-    list_dir = os.listdir(dataset_dir)
+    list_dir = sorted(os.listdir(dataset_dir))
     for i in range(int(len(list_dir))):
         # for i in range(35,140):
-        list_dir1 = os.listdir(dataset_dir + '/' + list_dir[i])
-        pkl_dir_save = path_pkl + '/' + list_dir[i]
-        if not os.path.exists(pkl_dir_save):
-            os.makedirs(pkl_dir_save)
+        list_dir1 = sorted(os.listdir(dataset_dir + '/' + list_dir[i]))
+
+
 
         for j in range(int(len(list_dir1))):
             path_to_im = dataset_dir + '/' + list_dir[i] + '/' + list_dir1[j]
-            path_to_save_pkl = (path_pkl + '/' + list_dir[i] + '/' + list_dir1[j]).replace("jpg", "pkl")
+            pkl_dir_save = path_pkl + '/' + list_dir[i] + '/' + list_dir1[j]
+            if not os.path.exists(pkl_dir_save):
+                os.makedirs(pkl_dir_save)
+            for im in sorted(os.listdir(path_to_im)):
+                im_path = path_to_im + '/' + im
+                path_to_save_pkl = (path_pkl + '/' + list_dir[i] + '/' + list_dir1[j] + '/' + im).replace("jpg", "pkl")
 
-            # load an image from file
-            image = load_img(path_to_im, target_size=(224, 224))
-            # convert the image pixels to a numpy array
-            image = img_to_array(image)
-            # reshape data for the model
-            image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-            # prepare the image for the VGG model
-            image = preprocess_input(image)          
-            # get extracted features
-            features = model.predict(image)
-            # save to file
-            dump(features, open(path_to_save_pkl, 'wb'))
+                # load an image from file
+                image = load_img(im_path, target_size=(224, 224))
+                # convert the image pixels to a numpy array
+                image = img_to_array(image)
+                # reshape data for the model
+                image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+                # prepare the image for the VGG model
+                image = preprocess_input(image)
+                # get extracted features
+                features = model.predict(image)
+                # save to file
+                dump(features, open(path_to_save_pkl, 'wb'))
+
+
 dataset_dir = '/media/bousefsa1/My Passport/BD PPG/2 bases publiques/ubfc_phys_frames'
 path_pkl = '/media/bousefsa1/My Passport/BD PPG/2 bases publiques/ubfc_phys_pkl'
 
